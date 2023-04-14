4d6c63fe882b348df71fdbb0bebf6286ef237ffb
邱土佳 <oncwnuALZwpGKnllCw7SNwrCQCJY@git.weixin.qq.com>
2023-04-14 15:11:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import {TemperatureControl} from './components/index'
+import { TemperatureControl}  from './components/index'
 </script>
-
 <template>
   <TemperatureControl  :value="23" :index="1" :width="250" :height="250" :unit="'℃'" :minimum="16" :maximum="36" />
 </template>
